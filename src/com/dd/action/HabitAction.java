ca6ac058494be16408f96046dd51fa3e3dd59788
zy337 <50911787+zy337@users.noreply.github.com>
2019-12-01 10:50:11 +0800

    Update HabitAction.java

--- src/com/dd/action/HabitAction.java
+++ src/com/dd/action/HabitAction.java
@@ -38,6 +38,7 @@ public class HabitAction extends ActionSupport{
 	}
 	
 	public String addHabit(){
+		habit.setFinishedNum(0);
 		habitDao.addHabit(habit);
 		return "message";
 	}
@@ -46,4 +47,26 @@ public class HabitAction extends ActionSupport{
 		habitList=habitDao.QueryAllHabit();
 		return "show_habit";
 	}
+	   public String editHabit() throws Exception {
+	    	System.out.println("编辑习惯");
+	    	Habit h=habitDao.GetHabitById(habit.getHid());
+	    	habit.setFinishedNum(h.getFinishedNum());
+	    	habitDao.UpdateHabit(habit);
+	        return "edit_message";
+	    }
+    public String showEdit() throws Exception {
+    	habit = habitDao.GetHabitById(habit.getHid());
+        return "edit_view";
+    }
+    public String daka() throws Exception {
+    	Habit h=habitDao.GetHabitById(habit.getHid());
+    	h.setFinishedNum(h.getFinishedNum()+1);
+    	habitDao.UpdateHabit(h);
+    	System.out.println("成功打卡一个习惯");
+        return "daka_success";
+    }
+    public String deleteHabit() throws Exception {
+    	habitDao.deleteHabit(habit.getHid());
+        return "delete_message";
+    }
 }
