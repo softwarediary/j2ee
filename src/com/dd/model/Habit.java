27c25a2a2e80b95c2385501cd5676f0b21dd5cbe
mxm696 <50907053+mxm696@users.noreply.github.com>
2019-12-21 18:45:46 +0800

    Update Habit.java
    可以自己建立映射文件，数据库加上外键

--- src/com/dd/model/Habit.java
+++ src/com/dd/model/Habit.java
@@ -4,167 +4,182 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-
 /**
  * Habit entity. @author MyEclipse Persistence Tools
  */
 
-public class Habit  implements java.io.Serializable {
+public class Habit implements java.io.Serializable {
 
+	// Fields
 
-    // Fields    
+	private Integer hid;
+	private User user;
+	private String hname;
+	private String pic;
+	private Integer totalNum;
+	private Integer finishedNum;
+	private String htype;
+	private String fre;
+	private String htext;
+	private Integer curdays;
+	private Integer aimdays;
+	private Integer hvalue;
+	private Date credate;
+	private Set dakas = new HashSet(0);
+	private Set dakas_1 = new HashSet(0);
 
-     private Integer hid;
-     private String hname;
-     private String pic;
-     private Integer totalNum;
-     private Integer finishedNum;
-     private String htype;
-     private String fre;
-     private String htext;
-     private Integer curdays;
-     private Integer aimdays;
-     private Integer hvalue;
-     private Date credate;
-     private Set dakas = new HashSet(0);
+	// Constructors
 
+	/** default constructor */
+	public Habit() {
+	}
 
-    // Constructors
+	/** minimal constructor */
+	public Habit(User user) {
+		this.user = user;
+	}
 
-    /** default constructor */
-    public Habit() {
-    }
+	/** full constructor */
+	public Habit(User user, String hname, String pic, Integer totalNum,
+			Integer finishedNum, String htype, String fre, String htext,
+			Integer curdays, Integer aimdays, Integer hvalue, Date credate,
+			Set dakas, Set dakas_1) {
+		this.user = user;
+		this.hname = hname;
+		this.pic = pic;
+		this.totalNum = totalNum;
+		this.finishedNum = finishedNum;
+		this.htype = htype;
+		this.fre = fre;
+		this.htext = htext;
+		this.curdays = curdays;
+		this.aimdays = aimdays;
+		this.hvalue = hvalue;
+		this.credate = credate;
+		this.dakas = dakas;
+		this.dakas_1 = dakas_1;
+	}
 
-    
-    /** full constructor */
-    public Habit(String hname, String pic, Integer totalNum, Integer finishedNum, String htype, String fre, String htext, Integer curdays, Integer aimdays, Integer hvalue, Date credate, Set dakas) {
-        this.hname = hname;
-        this.pic = pic;
-        this.totalNum = totalNum;
-        this.finishedNum = finishedNum;
-        this.htype = htype;
-        this.fre = fre;
-        this.htext = htext;
-        this.curdays = curdays;
-        this.aimdays = aimdays;
-        this.hvalue = hvalue;
-        this.credate = credate;
-        this.dakas = dakas;
-    }
+	// Property accessors
 
-   
-    // Property accessors
+	public Integer getHid() {
+		return this.hid;
+	}
 
-    public Integer getHid() {
-        return this.hid;
-    }
-    
-    public void setHid(Integer hid) {
-        this.hid = hid;
-    }
+	public void setHid(Integer hid) {
+		this.hid = hid;
+	}
 
-    public String getHname() {
-        return this.hname;
-    }
-    
-    public void setHname(String hname) {
-        this.hname = hname;
-    }
+	public User getUser() {
+		return this.user;
+	}
 
-    public String getPic() {
-        return this.pic;
-    }
-    
-    public void setPic(String pic) {
-        this.pic = pic;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public Integer getTotalNum() {
-        return this.totalNum;
-    }
-    
-    public void setTotalNum(Integer totalNum) {
-        this.totalNum = totalNum;
-    }
+	public String getHname() {
+		return this.hname;
+	}
 
-    public Integer getFinishedNum() {
-        return this.finishedNum;
-    }
-    
-    public void setFinishedNum(Integer finishedNum) {
-        this.finishedNum = finishedNum;
-    }
+	public void setHname(String hname) {
+		this.hname = hname;
+	}
 
-    public String getHtype() {
-        return this.htype;
-    }
-    
-    public void setHtype(String htype) {
-        this.htype = htype;
-    }
+	public String getPic() {
+		return this.pic;
+	}
 
-    public String getFre() {
-        return this.fre;
-    }
-    
-    public void setFre(String fre) {
-        this.fre = fre;
-    }
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
 
-    public String getHtext() {
-        return this.htext;
-    }
-    
-    public void setHtext(String htext) {
-        this.htext = htext;
-    }
+	public Integer getTotalNum() {
+		return this.totalNum;
+	}
 
-    public Integer getCurdays() {
-        return this.curdays;
-    }
-    
-    public void setCurdays(Integer curdays) {
-        this.curdays = curdays;
-    }
+	public void setTotalNum(Integer totalNum) {
+		this.totalNum = totalNum;
+	}
 
-    public Integer getAimdays() {
-        return this.aimdays;
-    }
-    
-    public void setAimdays(Integer aimdays) {
-        this.aimdays = aimdays;
-    }
+	public Integer getFinishedNum() {
+		return this.finishedNum;
+	}
 
-    public Integer getHvalue() {
-        return this.hvalue;
-    }
-    
-    public void setHvalue(Integer hvalue) {
-        this.hvalue = hvalue;
-    }
+	public void setFinishedNum(Integer finishedNum) {
+		this.finishedNum = finishedNum;
+	}
 
-    public Date getCredate() {
-        return this.credate;
-    }
-    
-    public void setCredate(Date credate) {
-        this.credate = credate;
-    }
+	public String getHtype() {
+		return this.htype;
+	}
 
-    public Set getDakas() {
-        return this.dakas;
-    }
-    
-    public void setDakas(Set dakas) {
-        this.dakas = dakas;
-    }
-   
+	public void setHtype(String htype) {
+		this.htype = htype;
+	}
 
+	public String getFre() {
+		return this.fre;
+	}
 
+	public void setFre(String fre) {
+		this.fre = fre;
+	}
 
+	public String getHtext() {
+		return this.htext;
+	}
 
+	public void setHtext(String htext) {
+		this.htext = htext;
+	}
 
+	public Integer getCurdays() {
+		return this.curdays;
+	}
 
+	public void setCurdays(Integer curdays) {
+		this.curdays = curdays;
+	}
 
+	public Integer getAimdays() {
+		return this.aimdays;
+	}
+
+	public void setAimdays(Integer aimdays) {
+		this.aimdays = aimdays;
+	}
+
+	public Integer getHvalue() {
+		return this.hvalue;
+	}
+
+	public void setHvalue(Integer hvalue) {
+		this.hvalue = hvalue;
+	}
+
+	public Date getCredate() {
+		return this.credate;
+	}
+
+	public void setCredate(Date credate) {
+		this.credate = credate;
+	}
+
+	public Set getDakas() {
+		return this.dakas;
+	}
+
+	public void setDakas(Set dakas) {
+		this.dakas = dakas;
+	}
+
+	public Set getDakas_1() {
+		return this.dakas_1;
+	}
+
+	public void setDakas_1(Set dakas_1) {
+		this.dakas_1 = dakas_1;
+	}
 
 }
