99b2ada68eef7b1a0f94c5c54fb941db2692289e
mxm696 <50907053+mxm696@users.noreply.github.com>
2019-12-21 18:43:42 +0800

    Update HabitAction.java

--- src/com/dd/action/HabitAction.java
+++ src/com/dd/action/HabitAction.java
@@ -8,16 +8,29 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.dd.dao.HabitDao;
+import com.dd.dao.Userdao;
 import com.dd.model.Habit;
 import com.dd.model.Reward;
+import com.dd.model.User;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 @Controller @Scope("prototype")
 public class HabitAction extends ActionSupport{
 @Resource HabitDao habitDao;
+@Resource Userdao userDao;
 	
 	private Habit habit;
+	private User user;
+	
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 	public Habit getHabit() {
 		return habit;
@@ -38,38 +51,45 @@ public class HabitAction extends ActionSupport{
 	}
 	
 	public String addHabit(){
+		user=userDao.QueryUserInfo(user.getUname()).get(0);
+		habit.setUser(user);
 		habit.setFinishedNum(0);
 		habitDao.addHabit(habit);
 		return "message";
 	}
 	
 	public String showHabit(){
-		habitList=habitDao.QueryAllHabit();
+		User u=userDao.QueryUserInfo(user.getUname()).get(0);
+		habitList=habitDao.QueryAllHabit(u);
 		return "show_habit";
 	}
-	   public String editHabit() throws Exception {
+	
+	 public String editHabit() throws Exception {
 	    	System.out.println("编辑习惯");
 	    	Habit h=habitDao.GetHabitById(habit.getHid());
+	    	user=userDao.QueryUserInfo(user.getUname()).get(0);
+			habit.setUser(user);
 	    	habit.setFinishedNum(h.getFinishedNum());
 	    	habitDao.UpdateHabit(habit);
 	        return "edit_message";
 	    }
-    public String showEdit() throws Exception {
-    	habit = habitDao.GetHabitById(habit.getHid());
-        return "edit_view";
-    }
-    public String daka() throws Exception {
-    	Habit h=habitDao.GetHabitById(habit.getHid());
-    	h.setFinishedNum(h.getFinishedNum()+1);
-    	habitDao.UpdateHabit(h);
-    	System.out.println("成功打卡一个习惯");
-        return "daka_success";
-    }
-    public String deleteHabit() throws Exception {
-    	habitDao.deleteHabit(habit.getHid());
-        return "delete_message";
-    }
-	private String hname;
+	 public String showEdit() throws Exception {
+		 habit = habitDao.GetHabitById(habit.getHid());
+		 return "edit_view";
+	 }
+	 public String daka() throws Exception {
+		 Habit h=habitDao.GetHabitById(habit.getHid());
+		 h.setFinishedNum(h.getFinishedNum()+1);
+		 habitDao.UpdateHabit(h);
+		 System.out.println("成功打卡一个习惯");
+		 return "daka_success";
+	 }
+	 public String deleteHabit() throws Exception {
+		 habitDao.deleteHabit(habit.getHid());
+		 return "delete_message";
+	 }
+  
+	 private String hname;
 	 public String getHname(){
 		 return hname;
 	 }
@@ -78,11 +98,12 @@ public class HabitAction extends ActionSupport{
 	 }
 	 
 	 public String selectHabit(){
-		 habitList=habitDao.QueryHabit(hname);
+		 User u=userDao.QueryUserInfo(user.getUname()).get(0);
+		 habitList=habitDao.QueryHabit(u,hname);
 		 return "show_habit";
 	 }
-	
-	public String showDetail(){
+	 
+	 public String showDetail(){
 		 habit=habitDao.GetHabitById(habit.getHid());
 		 return "detail_view";
 	 }
